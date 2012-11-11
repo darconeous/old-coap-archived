f24d007b76cbda445afabcc59808636b722f5079
Robert Quattlebaum <darco@deepdarc.com>
2012-11-10 16:49:00 -0800

    Make libtoolize copy rather than symlink

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 set -x
-libtoolize --force --install || exit -1
+libtoolize --copy --force --install || exit -1
 aclocal -I m4 || exit -1
 autoheader || exit -1
 automake --force-missing --foreign --add-missing --copy || exit -1
